2cf01f82bb011086e1088d3c007a7e67ef23839c
Hugo Kraft <huk@wificampus-031233.grenet.fr>
2022-02-16 16:16:54 +0100

    added addRoutine to routine service

--- src/app/services/routine.service.ts
+++ src/app/services/routine.service.ts
@@ -7,10 +7,18 @@ import { Routine } from '../models/routine';
   providedIn: 'root'
 })
 export class RoutineService {
-  // routines: Observable<Routine[]> = EMPTY;
-  // routinesCollection: AngularFirestoreCollection<Routine>;
+  routines: Observable<Routine[]> = EMPTY;
+  routinesCollection: AngularFirestoreCollection<Routine>;
 
   constructor(private afs: AngularFirestore) {
-    // this.routinesCollection = this.ads.collection<Routine>('list');
+    this.routinesCollection = this.afs.collection<Routine>('routines');
    }
+
+  getAll(){
+    return this.routines;
+  }
+
+  addRoutine(routine: Routine){
+    return this.routinesCollection.add(Object.assign({},routine))
+  }
 }
